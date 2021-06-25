59fda508a9abe2bf4859664a4641c92c4df66cba
Pozharitsky <s.pozharitsky@intelico.su>
2021-06-25 16:05:56 +0300

    fix: changed the structure of the table rendering

--- ts_test_task/components/Table/Table.tsx
+++ ts_test_task/components/Table/Table.tsx
@@ -1,9 +1,13 @@
 import { TableProps } from "./Table";
 import styles from "./table.module.css";
 
-const table = require("../JSON/Table.json");
+const table = require("../JSON/dataUrl.json");
 
 export const Table = ({ children, ...props }: TableProps): JSX.Element => {
+
+// const getTotalValues = value => {
+
+// }
 
   return (
     <div className={styles.sсrollableTableBox}>
@@ -17,9 +21,14 @@ export const Table = ({ children, ...props }: TableProps): JSX.Element => {
         </thead>
         <tbody>
           {table.rows.map((row, rowIndex) => {
-            return <tr key={rowIndex}>{row.cols.map((col, colIndex) => {
-              return <td key={colIndex}>{col}</td>;
-            })}</tr>;
+            return (
+              <tr key={rowIndex}>
+                {row?.cols?.map((col, colIndex) => {
+                  //if(col.id) getTotalValues(col.value)
+                  return <td key={colIndex}>{col.value}</td>;
+                })}
+              </tr>
+            );
           })}
         </tbody>
       </table>
