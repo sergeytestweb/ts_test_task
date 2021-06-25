f4d686163a29fa35a058c147c23df811b7eb5101
Sergey Pozharitsky <pozhar511@gmail.com>
2021-06-25 20:18:46 +0300

    feat: added style

--- ts_test_task/src/pages/index.tsx
+++ ts_test_task/src/pages/index.tsx
@@ -6,10 +6,6 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Table />
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo aperiam, labore excepturi harum ut, maxime consequatur, magnam molestiae odio temporibus voluptatum. Consequuntur, dolores cum sint recusandae labore officiis reiciendis ratione?
-      </div>
-
     </>
   );
 }
